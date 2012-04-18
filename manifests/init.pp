9b48b2320e484ebd4da34be9abf4ac138c88c18d
Cody Dracars <cdracars@gmail.com>
2012-04-18 16:55:37 -0500

    changes to work in current enviroment

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,8 +8,36 @@ class jenkins {
   include jenkins::package
   include jenkins::service
   include jenkins::firewall
+  include jenkins::configure
 
   Class['jenkins::repo'] -> Class['jenkins::package']
-  -> Class['jenkins::service']
+  -> Class['jenkins::service'] -> Class['jenkins::configure']
+
+  jenkins::plugin {
+    'analysis-core':;
+    'checkstyle':;
+    'dry':;
+    'phing':;
+    'plot':;
+    'pmd':;
+    'build-timeout':;
+    'claim':;
+    'disk-usage':;
+    'email-ext':;
+    'favorite':;
+    'git':;
+    'envinject':;
+    'jobConfigHistory':;
+    'project-stats-plugin':;
+    'redmine':;
+    'seleniumhq':;
+    'statusmonitor':;
+    'instant-messaging':;
+    'jabber':;
+    'tasks':;
+    'warnings':;
+  }
+
 }
-# vim: ts=2 et sw=2 autoindent
+
+
