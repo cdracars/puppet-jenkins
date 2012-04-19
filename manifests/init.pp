2e39a099198c6f7f75655756ecb15217254c6566
Cody Dracars <cdracars@gmail.com>
2012-04-19 16:37:23 -0500

    My Changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,33 +10,8 @@ class jenkins {
   include jenkins::firewall
   include jenkins::configure
 
-  Class['jenkins::repo'] -> Class['jenkins::package']
-  -> Class['jenkins::service'] -> Class['jenkins::configure']
-
-  jenkins::plugin {
-    'analysis-core':;
-    'checkstyle':;
-    'dry':;
-    'phing':;
-    'plot':;
-    'pmd':;
-    'build-timeout':;
-    'claim':;
-    'disk-usage':;
-    'email-ext':;
-    'favorite':;
-    'git':;
-    'envinject':;
-    'jobConfigHistory':;
-    'project-stats-plugin':;
-    'redmine':;
-    'seleniumhq':;
-    'statusmonitor':;
-    'instant-messaging':;
-    'jabber':;
-    'tasks':;
-    'warnings':;
-  }
+  Class['jenkins::repo'] -> Class['jenkins::package'] 
+  -> Class['jenkins::configure'] -> Class['jenkins::service']
 
 }
 
